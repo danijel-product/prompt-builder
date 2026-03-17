294d518b7ef85e2fad09e0431c1b1aefeba7dec7
Danijel <danijelgojkovic5@gmail.com>
2026-03-17 17:27:06 +0100

    API updated

--- api/ask.js
+++ api/ask.js
@@ -5,23 +5,30 @@ const groq = new Groq({
 });
 
 export default async function handler(req, res) {
-
+    try {
   const { input, context, role, task, format } = req.body;
 
-  let prompt = input || `
-Context: ${context}
-Role: ${role}
-Task: ${task}
-Format: ${format}
-`;
+    let prompt
 
-  try {
+    if(input){
+        prompt = input
+    }else{
+    prompt = `
+    Context:${context}
+    Role:${role}
+    Task:${task}`
+
+    if(format){
+        prompt += `\n\nOdgovor OBAVEZNO vrati u sledećem formatu:\n${format}`
+    }
+    }
+
     const response = await groq.chat.completions.create({
       model: "llama-3.1-8b-instant",
       messages: [{ role: "user", content: prompt }],
     });
 
-    // sigurnosna provera
+
     if (!response?.choices?.[0]?.message?.content) {
       return res.status(500).json({ error: "Invalid response from Groq API" });
     }
